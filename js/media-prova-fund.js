043db3a1d6be960a836c70e36a4e3965d443074e
Marcel Henrique <marcelhrb@gmail.com>
2022-03-06 19:29:23 -0300

    started from elementary school calculator done

--- js/media-prova-fund.js
+++ js/media-prova-fund.js
@@ -1,19 +1,88 @@
-let interdisciplinarFUNDInput = document.querySelector('#interdisciplinar-FUND');
-let trimestralFUNDInput = document.querySelector('#trimestral-FUND');
+function calcularMediaProvaGrupoIFUND(){
 
-function calcularMediaProvaFUND(){
+    let interdisciplinarFUNDInput = document.querySelector('#interdisciplinar-FUND');
+    let PorAreaFUNDInput = document.querySelector('#PorArea-FUND')
+    let trimestralFUNDInput = document.querySelector('#trimestral-FUND');
 
     let interdisciplinarFUND = Number(interdisciplinarFUNDInput.value);
+    let PorAreaFUND = Number(PorAreaFUNDInput.value);
     let trimestralFUND = Number(trimestralFUNDInput.value);
 
-    let navProvasFUND = document.querySelector('#media-provas-FUND');
+    let navProvasFUND = document.querySelector('#mediaProvasFUND');
 
     if(interdisciplinarFUND > 10 || interdisciplinarFUND < 0){
         alert('Valor da sua Interdisciplinar esta inválido');
     }else if(trimestralFUND > 10 || interdisciplinarFUND < 0){
         alert('Valor da sua Trimestral esta inválido')
     }else{
-        let mediaProvasFUND = interdisciplinarFUND * 0.4 + trimestralFUND * 0.6;
+        let mediaProvasFUND = interdisciplinarFUND * 0.3 + PorAreaFUND * 0.3 + trimestralFUND * 0.4;
+        let backgroundMain = document.querySelector('main');
+        let backgroundHTML = document.querySelector('html');
+
+        if(mediaProvasFUND < 6.0){
+            backgroundMain.style.background = '#F25757';
+            backgroundHTML.style.background = '#F25757';
+            navProvasFUND.innerHTML = `Sua média é ${mediaProvasFUND.toFixed(2)}`;
+        }else{
+            backgroundMain.style.background = '#3EFFAC';
+            backgroundHTML.style.background = '#3EFFAC';
+            navProvasFUND.innerHTML = `Sua média é ${mediaProvasFUND.toFixed(2)}`
+        }
+    }
+}
+
+function calcularMediaProvaGrupoIIFUND(){
+
+    let interdisciplinarFUNDInput = document.querySelector('#interdisciplinar-FUND');
+    let simuladoProficienciaFUNDInput = document.querySelector('#simuladoProficiencia-FUND');
+    let trimestralFUNDInput = document.querySelector('#trimestral-FUND');
+
+    let interdisciplinarFUND = Number(interdisciplinarFUNDInput.value);
+    let simuladoProficienciaFUND = Number(simuladoProficienciaFUNDInput.value);
+    let trimestralFUND = Number(trimestralFUNDInput.value);
+
+    let navProvasFUND = document.querySelector('#mediaProvasFUND');
+
+    if(interdisciplinarFUND > 10 || interdisciplinarFUND < 0){
+        alert('Valor da sua Interdisciplinar esta inválido');
+    }else if(trimestralFUND > 10 || interdisciplinarFUND < 0){
+        alert('Valor da sua Trimestral esta inválido')
+    }else{
+        let mediaProvasFUND = interdisciplinarFUND * 0.3 + simuladoProficienciaFUND * 0.2 + trimestralFUND * 0.5;
+        let backgroundMain = document.querySelector('main');
+        let backgroundHTML = document.querySelector('html');
+
+        if(mediaProvasFUND < 6.0){
+            backgroundMain.style.background = '#F25757';
+            backgroundHTML.style.background = '#F25757';
+            navProvasFUND.innerHTML = `Sua média é ${mediaProvasFUND.toFixed(2)}`;
+        }else{
+            backgroundMain.style.background = '#3EFFAC';
+            backgroundHTML.style.background = '#3EFFAC';
+            navProvasFUND.innerHTML = `Sua média é ${mediaProvasFUND.toFixed(2)}`
+        }
+    }
+}
+
+function calcularMediaProvaGrupoIIIFUND(){
+
+    let interdisciplinarFUNDInput = document.querySelector('#interdisciplinar-FUND');
+    let avaliaçãoProcessualFUNDInput = document.querySelector('#simuladoProficiencia-FUND');
+    let autoavaliaçãoFUNDInput = document.querySelector('#autoavaliação-FUND')
+    let atividadeavaliativasFUNDInput = document.querySelector('#atividadeavaliativas-FUND');
+
+    let interdisciplinarFUND = Number(interdisciplinarFUNDInput.value);
+    let avaliaçãoProcessualFUND = Number(avaliaçãoProcessualFUNDInput.value);
+    let autoavaliaçãoFUND = Number(autoavaliaçãoFUNDInput.value);
+    let atividadeavaliativasFUND = Number(atividadeavaliativasFUNDInput.value);
+
+
+    let navProvasFUND = document.querySelector('#mediaProvasFUND');
+
+    if(interdisciplinarFUND > 10 || interdisciplinarFUND < 0){
+        alert('Valor da sua Interdisciplinar esta inválido');
+     }else{
+        let mediaProvasFUND = interdisciplinarFUND * 0.2 + avaliaçãoProcessualFUND * 0.5 + autoavaliaçãoFUND * 0.1 + atividadeavaliativasFUND*0.2;
         let backgroundMain = document.querySelector('main');
         let backgroundHTML = document.querySelector('html');
 
---
+++ js/show-inputs-fund.js
@@ -0,0 +1,129 @@
+var materiasFundInput = document.querySelector('#materiasFund');
+
+materiasFundInput.addEventListener('input', function materia(){
+
+    var materiasFund = materiasFundInput.value;
+
+    var forms = document.getElementById('forms-fund');
+
+    if(selecionarMateriaFUND(materiasFund) == 1){
+        forms.innerHTML = `
+            <nav>
+                <nav id="interdiciplinar" class="provas">
+                    <p>Coloque sua nota da avaliação interdisciplinar:</p>
+                    <input type="number" id="interdisciplinar-FUND">
+                </nav>
+                <nav>
+                    <p>Coloque sua avaliações parciais por área:</p>
+                    <input type="number" id="PorArea-FUND">
+                </nav>
+                <nav id="trimestral" class="provas">
+                    <p>Coloque sua nota da avaliação trimestral:</p>
+                    <input type="number" id="trimestral-FUND">
+                <a onclick="calcularMediaProvaGrupoIFUND()"><nav id="calcular-media">Calcular Média</nav></a>
+                </nav>
+                <nav id="mediaProvasFUND"></nav>
+            </nav>
+            <nav id="separacao"><nav></nav></nav>
+            <nav>
+                <p>Média das provas / Nota de recuperação</p>
+                <input type="number" id="av5-FUND">
+                <p>Geekie Teste:</p>
+                <input type="number" id="av6-FUND">
+                <p>Entrega e engajamento:</p>
+                <input type="number" id="av7-FUND">
+
+                <a onclick="mediaTrimestreFUND()"><nav id="calcular-media">Calcular Média do Trimestre</nav></a>
+            </nav>
+            <script src="media-prova-fund.js"></script>
+        `
+    }else if(selecionarMateriaFUND(materiasFund) == 2){
+        forms.innerHTML = `
+            <nav>
+            <nav id="interdiciplinar" class="provas">
+                <p>Coloque sua nota da avaliação interdisciplinar:</p>
+                <input type="number" id="interdisciplinar-FUND">
+            </nav>
+            <nav>
+                <p>Coloque sua nota do simulado de proficiência:</p>
+                <input type="number" id="simuladoProficiencia-FUND">
+            </nav>
+            <nav id="trimestral" class="provas">
+                <p>Coloque sua nota da avaliação trimestral:</p>
+                <input type="number" id="trimestral-FUND">
+            <a onclick="calcularMediaProvaGrupoIIFUND()"><nav id="calcular-media">Calcular Média</nav></a>
+            </nav>
+            <nav id="mediaProvasFUND"></nav>
+            </nav>
+            <nav id="separacao"><nav></nav></nav>
+            <nav>
+                <p>Média das provas / Nota de recuperação</p>
+                <input type="number" id="av5-FUND">
+                <p>Geekie Teste:</p>
+                <input type="number" id="av6-FUND">
+                <p>Entrega e engajamento:</p>
+                <input type="number" id="av7-FUND">
+
+                <a onclick="mediaTrimestreFUND()"><nav id="calcular-media">Calcular Média do Trimestre</nav></a>
+            </nav>
+            <script src="media-prova-fund.js"></script>
+        `
+    }else if(selecionarMateriaFUND(materiasFund) == 3){
+        forms.innerHTML = `
+            <nav>
+            <nav id="interdiciplinar" class="provas">
+                <p>Coloque sua nota da avaliação interdisciplinar:</p>
+                <input type="number" id="interdisciplinar-FUND">
+            </nav>
+            <nav>
+                <p>Coloque sua nota da avaliação processual:</p>
+                <input type="number" id="simuladoProficiencia-FUND">
+            </nav>
+            <nav>
+                <p>Coloque sua nota da Autoavaliação / autodiagnóstico e plano de Ação:</p>
+                <input type="number" id="autoavaliação-FUND">
+            </nav>
+            <nav id="trimestral" class="provas">
+                <p>Coloque sua nota das atividades avaliativas e/ou projeto:</p>
+                <input type="number" id="atividadeavaliativas-FUND">
+            <a onclick="calcularMediaProvaGrupoIIIFUND()"><nav id="calcular-media">Calcular Média</nav></a>
+            </nav>
+            <nav id="mediaProvasFUND"></nav>
+            </nav>
+            <nav id="separacao"><nav></nav></nav>
+            <nav>
+                <p>Média das provas / Nota de recuperação</p>
+                <input type="number" id="av5-FUND">
+                <p>Geekie Teste:</p>
+                <input type="number" id="av6-FUND">
+                <p>Entrega e engajamento:</p>
+                <input type="number" id="av7-FUND">
+
+                <a onclick="mediaTrimestreFUND()"><nav id="calcular-media">Calcular Média do Trimestre</nav></a>
+            </nav>
+            <script src="media-prova-fund.js"></script>
+        `
+    }else if(selecionarMateriaFUND(materiasFund) == 4){
+        forms.innerHTML = `<h1>grupo 4</h1>`
+    }
+
+})
+
+function selecionarMateriaFUND(materiasFund){
+
+    let grupo1 = ['RIPT','Gramática','Matemática I', 'Matemática II','História','Geografia','Ciências','Química/Física'];
+    let grupo2 = ['Inglês','Espanhol','Japonês'];
+    let grupo3 = ['Educação Física','Arte','Empreendedorismo Maker','Projeto de Vida','Música','Oratória'];
+    let grupo4 = ['Núcleos Eletivos Formativos', 'STEM', 'Educação Ambiental'];
+
+    if(grupo1.indexOf(materiasFund) > -1){
+        return 1
+    }else if(grupo2.indexOf(materiasFund) > -1){
+        return 2
+    }else if(grupo3.indexOf(materiasFund) > -1){
+        return 3
+    }else if(grupo4.indexOf(materiasFund) > -1){
+        return 4
+    }
+
+}
