824014fca50a1f022c8b52dd26613aeea0e0b1fe
Marcel Henrique <60407893+marcel1712@users.noreply.github.com>
2022-04-20 18:52:23 +0000

    .

--- js/quanto-falta-fundII.js
+++ js/quanto-falta-fundII.js
@@ -62,3 +62,37 @@ function CalculoQuantoFaltaFUNDPorArea(){
   }
   
 }
+
+function CalculoQuantoFaltaFUNDTrimestral() {
+
+  
+  let interdisciplinarFUNDInput = document.querySelector('#interdisciplinar-FUND');
+  let PorAreaFUNDInput = document.querySelector('#PorArea-FUND');
+  let trimestralFUNDInput = document.querySelector('#trimestral-FUND');
+
+  let interdisciplinarFUND = Number(interdisciplinarFUNDInput.value);
+  let PorAreaFUND = Number(PorAreaFUNDInput.value);
+  let trimestralFUND = Number(trimestralFUNDInput.value);
+
+  if(trimestralFUND == 0 && interdisciplinarFUND > 0 && PorAreaFUND > 0){
+    
+    let quantoFaltaTrimestralFUND = ((6 - ((interdisciplinarFUND * 0.3)+(PorAreaFUND*0.3)))/0.4);
+    return alert(`Você precisa tirar ${quantoFaltaTrimestralFUND.toFixed(2)} na trimestral para passar na AV4`);
+    
+  }else if(interdisciplinarFUND == 0 && PorAreaFUND == 0){
+    
+    let quantoFaltaInterdisciplinarFUND = (((6-(0.4*trimestralFUND))*(1/2))/0.3);
+    let quantoFaltaPorAreaFUND = (((6-(0.4*trimestralFUND))*(1/2))/0.3);
+    return alert(`Você precisa tirar ${quantoFaltaInterdisciplinarFUND.toFixed(2)} na interdisciplinar e ${quantoFaltaPorAreaFUND.toFixed(2)} na avaliação por área para passar na AV4`);
+    
+  }else if(interdisciplinarFUND > 0 && PorAreaFUND == 0){
+
+    let quantoFaltaPorAreaFUND = ((6 - ((trimestralFUND * 0.4)+(interdisciplinarFUND*0.3)))/0.3);
+    return alert(`Você precisa tirar ${quantoFaltaPorAreaFUND} na avaliação por área para passar na AV4`);
+    
+  }else if (PorAreaFUND > 0 && trimestralFUND == 0){
+    let quantoFaltaInterdisciplinarFUND = ((6 - ((trimestralFUND * 0.4)+(PorAreaFUND*0.3)))/0.3);
+    return alert(`Você precisa tirar ${quantoFaltaInterdisciplinarFUND} na interdisciplinar para passar na AV4`);
+  }
+  
+}
